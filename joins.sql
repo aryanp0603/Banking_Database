0c066c05b18d4f6ac29b17da208dd948b01e3a58
Aryan Patel <patelaryan632007@gmail.com>
2026-06-19 20:59:43 +0530

    outer joins

--- joins.sql
+++ joins.sql
@@ -18,16 +18,37 @@ Where transactionID In (308);
 
 select * from transactions;
 
--- customer :- id, first name, transaction id, type, amount
+-- customer :- id, first name, transaction id, type, amount 
+-- Inner join
 SELECT c.CustomerID, c.FirstName, t.TransactionID, t.TransactionType, t.Amount
 from customers as c inner join transactions as t
 on c.CustomerID= t.CustomerID;
 
-
+-- Inner join using condition
 SELECT c.CustomerID, c.FirstName, t.TransactionID, t.TransactionType, t.Amount
 from customers as c inner join transactions as t
 on c.CustomerID= t.CustomerID
 WHERE TransactionType In ("Withdrawal", "Deposit");
+
+select c.CustomerID, Concat(c.FirstName, " ", c.LastName) AS CustomerName,t.TransactionID,t.TransactionType,t.Amount
+From Customers as c
+left join transactions as t
+on c.CustomerID= t.CustomerID;
+
+update transactions set CustomerID= Null 
+Where TransactionID= 310;
+
+select c.CustomerID, Concat(c.FirstName, " ", c.LastName) AS CustomerName,t.TransactionID,t.TransactionType,t.Amount
+From Customers as c
+right join transactions as t
+on c.CustomerID= t.CustomerID;
+
+select c.CustomerID, Concat(c.FirstName, " ", c.LastName) AS CustomerName,t.TransactionID,t.TransactionType,t.Amount
+From Customers as c
+inner join transactions as t
+on c.CustomerID= t.CustomerID
+Where t.transactiontype= "Deposit"
+and t.amount > 5000;
 
 
 
